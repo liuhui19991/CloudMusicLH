d6df7dcd02be965c6fbc9736cbe692fc08cb79f6
liuhui19991 <396604715@qq.com>
2016-07-15 15:19:52 +0800

    怎家自定义的圆环进度

--- cloudmusic/src/main/java/com/carporange/cloudmusic/fragment/SongMenuFragment.java
+++ cloudmusic/src/main/java/com/carporange/cloudmusic/fragment/SongMenuFragment.java
@@ -1,22 +1,66 @@
 package com.carporange.cloudmusic.fragment;
 
 
+import android.graphics.Color;
+
 import com.carporange.cloudmusic.R;
 import com.carporange.cloudmusic.ui.base.BaseFragment;
+import com.carporange.cloudmusic.widget.CircleTextProgressbar;
+
+import butterknife.BindView;
 
 /**
  * Created by liuhui on 2016/6/27.
  */
 public class SongMenuFragment extends BaseFragment {
-
+    @BindView(R.id.circle_progress)
+    CircleTextProgressbar mCircleProgress;
+    @BindView(R.id.circle_progress_opposite)
+    CircleTextProgressbar mCircleProgressOpposite;
 
     public SongMenuFragment() {
     }
 
     @Override
-    protected void onVisible() {
+    protected void initViews() {
+        // 和系统普通进度条一样，0-100。顺时进度条
+        mCircleProgress.setProgressType(CircleTextProgressbar.ProgressType.COUNT);
+        // 改变进度条。
+        mCircleProgress.setProgressLineWidth(26);// 进度条宽度。
 
+        // 设置倒计时时间毫秒，默认3000毫秒。
+        mCircleProgress.setTimeMillis(2000);
+
+        // 改变进度条颜色。
+        mCircleProgress.setProgressColor(Color.parseColor("#AA66C6C6"));
+
+        // 改变外部边框颜色。
+        mCircleProgress.setOutLineColor(Color.TRANSPARENT);
+
+        // 改变圆心颜色。
+        mCircleProgress.setInCircleColor(Color.TRANSPARENT);
     }
+
+    @Override
+    protected void onVisible() {
+        // 如果需要自动倒计时，就会自动走进度。
+//        mCircleProgress.start();//只能开启一次
+        mCircleProgress.reStart();
+        mCircleProgressOpposite.reStart();
+        mCircleProgress.setCountdownProgressListener(1, onCountdownProgressListener);
+        mCircleProgressOpposite.setCountdownProgressListener(2, onCountdownProgressListener);
+    }
+
+    CircleTextProgressbar.OnCountdownProgressListener onCountdownProgressListener = new CircleTextProgressbar.OnCountdownProgressListener() {
+        @Override
+        public void onProgress(int what, int progress) {
+            if (what == 1) {
+                mCircleProgress.setText(progress + "%");
+            } else if (what == 2) {
+                mCircleProgressOpposite.setText(progress + "%");
+            }
+        }
+    };
 
     @Override
     protected int getLayoutId() {
