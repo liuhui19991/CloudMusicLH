fac51395ebbf85695f395265dee14a51ef27e839
liuhui19991 <396604715@qq.com>
2016-07-18 16:19:04 +0800

    增加控件CardView

--- cloudmusic/src/main/java/com/carporange/cloudmusic/fragment/PersonalRecommendationFragment.java
+++ cloudmusic/src/main/java/com/carporange/cloudmusic/fragment/PersonalRecommendationFragment.java
@@ -1,21 +1,90 @@
 package com.carporange.cloudmusic.fragment;
 
 
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomSheetBehavior;
+import android.support.design.widget.BottomSheetDialog;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+
 import com.carporange.cloudmusic.R;
+import com.carporange.cloudmusic.adapter.ListRecyclerAdapter;
 import com.carporange.cloudmusic.ui.base.BaseFragment;
+import com.carporange.cloudmusic.util.S;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.OnClick;
 
 /**
  * Created by liuhui on 2016/6/27.
  */
 public class PersonalRecommendationFragment extends BaseFragment {
-
+    private BottomSheetDialog mBottomSheetDialog;
 
     public PersonalRecommendationFragment() {
     }
 
     @Override
     protected void onVisible() {
+    }
 
+    @Override
+    protected void initViews() {
+        createBottomSheetDialog();
+
+    }
+    private void createBottomSheetDialog() {
+        mBottomSheetDialog = new BottomSheetDialog(getActivity());
+        View view = LayoutInflater.from(getActivity()).inflate(R.layout.dialog_bottom_sheet, null, false);
+        mBottomSheetDialog.setContentView(view);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < 20; i++) {
+            list.add("我是第" + i + "个");
+        }
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        linearLayoutManager.setSmoothScrollbarEnabled(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        ListRecyclerAdapter adapter = new ListRecyclerAdapter(list);
+        recyclerView.setAdapter(adapter);
+
+        setBehaviorCallback();
+    }
+
+    private void setBehaviorCallback() {
+        View view = mBottomSheetDialog.getDelegate().findViewById(android.support.design.R.id.design_bottom_sheet);
+        final BottomSheetBehavior bottomSheetBehavior = BottomSheetBehavior.from(view);
+        bottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+            @Override
+            public void onStateChanged(@NonNull View bottomSheet, int newState) {
+                if (newState == BottomSheetBehavior.STATE_HIDDEN) {
+                    mBottomSheetDialog.dismiss();
+                    bottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                }
+            }
+
+            @Override
+            public void onSlide(@NonNull View bottomSheet, float slideOffset) {
+            }
+        });
+    }
+
+    /**
+     * 显示dialog的按钮点击事件
+     */
+    @OnClick(R.id.btn_bottom_dialog_control)
+    public void dialog() {
+        if (mBottomSheetDialog.isShowing()) {
+            mBottomSheetDialog.dismiss();
+        } else {
+            mBottomSheetDialog.show();
+        }
     }
 
     @Override
