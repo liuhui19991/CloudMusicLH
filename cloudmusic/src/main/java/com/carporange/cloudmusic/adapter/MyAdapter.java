1122a7e55d4b86a7c743f0441ece0aa38260e587
liuhui19991 <396604715@qq.com>
2016-07-21 13:16:16 +0800

    顶部滑动隐藏

--- cloudmusic/src/main/java/com/carporange/cloudmusic/adapter/MyAdapter.java
+++ cloudmusic/src/main/java/com/carporange/cloudmusic/adapter/MyAdapter.java
@@ -1,39 +1,68 @@
 package com.carporange.cloudmusic.adapter;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.carporange.cloudmusic.R;
+import com.nineoldandroids.view.ViewHelper;
+import com.nineoldandroids.view.ViewPropertyAnimator;
 
 import java.util.ArrayList;
-import java.util.List;
 
 /**
  * Created by liuhui on 2016/7/21.
  */
 public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     private ArrayList<String> datas;
+
     public MyAdapter(ArrayList mlist) {
-        datas=  mlist;
+        datas = mlist;
     }
 
+    /**
+     * 创建新View，被LayoutManager所调用
+     *
+     * @param parent
+     * @param viewType
+     * @return
+     */
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_beautful, parent, false);
+        return new ViewHolder(view);
     }
 
+    /**
+     * 将数据与界面进行绑定的操作
+     *
+     * @param holder
+     * @param position
+     */
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
-
+//	        ViewHelper.setScaleX(holder.mTextView, 0.5f);
+//	        ViewHelper.setScaleY(holder.mTextView, 0.5f);
+//	        ViewHelper.setAlpha(holder.mTextView, 0.5f);
+//	        ViewPropertyAnimator.animate(holder.mTextView).scaleX(1.0f).setDuration(500).start();
+//	        ViewPropertyAnimator.animate(holder.mTextView).scaleY(1.0f).setDuration(500).start();
+//	        ViewPropertyAnimator.animate(holder.mTextView).alpha(1.0f).setDuration(500).start();
+        holder.mTextView.setText(datas.get(position));
     }
 
+    /**
+     * @return 获取数据的数量
+     */
     @Override
     public int getItemCount() {
-        return 0;
+        return datas.size();
     }
 
+    /**
+     * 自定义的ViewHolder，持有每个Item的的所有界面元素
+     */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         public TextView mTextView;
 
--- cloudmusic/src/main/java/com/carporange/cloudmusic/ui/activity/BeautfulActivity.java
+++ cloudmusic/src/main/java/com/carporange/cloudmusic/ui/activity/BeautfulActivity.java
@@ -1,19 +1,22 @@
 package com.carporange.cloudmusic.ui.activity;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.carporange.cloudmusic.R;
 import com.carporange.cloudmusic.adapter.MyAdapter;
+import com.carporange.cloudmusic.util.L;
+import com.carporange.cloudmusic.util.S;
 import com.jcodecraeer.xrecyclerview.ProgressStyle;
 import com.jcodecraeer.xrecyclerview.XRecyclerView;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -26,42 +29,84 @@ public class BeautfulActivity extends AppCompatActivity {
     XRecyclerView mXRecyclerView;
     private ArrayList<String> mListData;
     private MyAdapter mAdapter;
+    private int refreshTime, times;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_beautful);
         ButterKnife.bind(this);
-        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        final Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         initView();
     }
 
     private void initView() {
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
-        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);//默认垂直排列,可以不用写
+//        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);//默认垂直排列,可以不用写
         mXRecyclerView.setLayoutManager(linearLayoutManager);
 
         mXRecyclerView.setRefreshProgressStyle(ProgressStyle.BallSpinFadeLoader);
-        mXRecyclerView.setLoadingMoreProgressStyle(ProgressStyle.BallRotate);
-        mXRecyclerView.setArrowImageView(R.mipmap.iconfont_downgrey);//
+        mXRecyclerView.setLoadingMoreProgressStyle(ProgressStyle.BallGridBeat);
+//        mXRecyclerView.setPullRefreshEnabled(false);//不允许下拉刷新
+//        mXRecyclerView.setArrowImageView(R.mipmap.iconfont_downgrey);//更换刷新箭头
         mXRecyclerView.setLoadingListener(new XRecyclerView.LoadingListener() {
             @Override
             public void onRefresh() {
-
+                refreshTime++;
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        mListData.clear();
+                        for (int i = 0; i < 15; i++) {
+                            mListData.add("item" + i + "after " + refreshTime + " times of refresh");
+                        }
+                        mXRecyclerView.refreshComplete();
+                        mAdapter.notifyDataSetChanged();
+                    }
+                }, 800);
             }
 
             @Override
             public void onLoadMore() {
-
+                if (times < 2) {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            for (int i = 0; i < 15; i++) {
+                                mListData.add("item" + (i + mListData.size()));
+                            }
+                            mXRecyclerView.loadMoreComplete();
+                            mAdapter.notifyDataSetChanged();
+                        }
+                    }, 800);
+                } else {
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            for (int i = 0; i < 8; i++) {
+                                mListData.add("lastData" + (i + mListData.size()));
+                            }
+                            mXRecyclerView.setIsnomore(true);
+                            mAdapter.notifyDataSetChanged();
+                        }
+                    }, 800);
+                }
+                times++;
             }
         });
         mListData = new ArrayList<>();
         for (int i = 0; i < 10; i++) {
             mListData.add("RecyclerView" + i);
         }
+//        View header =   LayoutInflater.from(this).inflate(R.layout.recyclerview_header, (ViewGroup)findViewById(android.R.id.content),false);
+//        View header1 =   LayoutInflater.from(this).inflate(R.layout.recyclerview_header1, (ViewGroup)findViewById(android.R.id.content),false);
+//        mXRecyclerView.addHeaderView(header1);
+//        mXRecyclerView.addHeaderView(header);
         mAdapter = new MyAdapter(mListData);
         mXRecyclerView.setAdapter(mAdapter);
+
     }
 
     /**
