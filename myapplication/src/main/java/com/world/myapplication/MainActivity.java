bedcf045a29474982b41331e6509d7eb7cbf4029
liuhui19991 <396604715@qq.com>
2016-08-17 16:06:22 +0800

    js交互

---
+++ myapplication/src/main/java/com/world/myapplication/MainActivity.java
@@ -0,0 +1,74 @@
+package com.world.myapplication;
+
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.JavascriptInterface;
+import android.webkit.WebView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private WebView contentWebView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        contentWebView = (WebView) findViewById(R.id.webview);
+        // 启用javascript
+        contentWebView.getSettings().setJavaScriptEnabled(true);
+        // 从assets目录下面的加载html
+        contentWebView.loadUrl("file:///android_asset/web.html");
+        contentWebView.addJavascriptInterface(this, "android");
+
+
+        //Button按钮 无参调用HTML js方法
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 无参数调用 JS的方法
+                contentWebView.loadUrl("javascript:javacalljs()");
+
+            }
+        });
+        //Button按钮 有参调用HTML js方法
+        findViewById(R.id.button2).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 传递参数调用JS的方法
+                contentWebView.loadUrl("javascript:javacalljswith(" + "'http://blog.csdn.net/Leejizhou'" + ")");
+            }
+        });
+
+
+    }
+
+    //由于安全原因 targetSdkVersion>=17需要加 @JavascriptInterface
+    //JS调用Android JAVA方法名和HTML中的按钮 onclick后的别名后面的名字对应
+    @JavascriptInterface
+    public void startFunction() {
+
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, "show", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    @JavascriptInterface
+    public void startFunction(final String text) {
+        runOnUiThread(new Runnable() {
+
+            @Override
+            public void run() {
+                new AlertDialog.Builder(MainActivity.this).setMessage(text).show();
+
+            }
+        });
+
+
+    }
+}
