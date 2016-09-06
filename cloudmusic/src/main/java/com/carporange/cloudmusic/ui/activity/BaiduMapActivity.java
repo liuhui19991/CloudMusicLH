566715b2594cac9f3e11b9b5dfb39a2a75897bde
liuhui19991 <396604715@qq.com>
2016-09-06 17:27:46 +0800

    完成百度地图定位

--- cloudmusic/src/main/java/com/carporange/cloudmusic/ui/activity/BaiduMapActivity.java
+++ cloudmusic/src/main/java/com/carporange/cloudmusic/ui/activity/BaiduMapActivity.java
@@ -1,22 +1,239 @@
 package com.carporange.cloudmusic.ui.activity;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.BitmapDescriptor;
+import com.baidu.mapapi.map.BitmapDescriptorFactory;
+import com.baidu.mapapi.map.MapStatus;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.Marker;
+import com.baidu.mapapi.map.MarkerOptions;
+import com.baidu.mapapi.map.MyLocationConfiguration;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.map.OverlayOptions;
+import com.baidu.mapapi.map.PolygonOptions;
+import com.baidu.mapapi.map.Stroke;
+import com.baidu.mapapi.model.LatLng;
+import com.carporange.cloudmusic.R;
 import com.carporange.cloudmusic.ui.base.BaseActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by liuhui on 2016/9/5.
  */
-public class BaiduMapActivity extends BaseActivity{
+public class BaiduMapActivity extends BaseActivity {
+    //获取地图控件引用
+    private MapView mMapView;
+    private BaiduMap mBaiduMap;
+    // 定位相关
+    private LocationClient mLocClient;
+    private boolean isFirstLoc = true; // 是否首次定位
+    private MyLocationListenner myListener = new MyLocationListenner();
+    private static final int BAIDU_READ_PHONE_STATE = 100;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        //在使用SDK各组件之前初始化context信息，传入ApplicationContext
+        //注意该方法要再setContentView方法之前实现
+        SDKInitializer.initialize(getApplicationContext());
+        super.onCreate(savedInstanceState);
+    }
+
     @Override
     protected int getLayoutId() {
-        return 0;
+        return R.layout.activity_baidumap;
     }
 
     @Override
     public void initViews() {
+//        if (Build.VERSION.SDK_INT >= 23) {
+//            showPressmion();
+//        } else {
+//        }
+            init();
+    }
 
+    private void showPressmion() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED
+                || ActivityCompat.checkSelfPermission(this, Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Contacts permissions have not been granted.
+            requestContactsPermissions();
+
+        } else {
+            init();
+        }
+    }
+
+    private void requestContactsPermissions() {
+        // BEGIN_INCLUDE(contacts_permission_request)
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_COARSE_LOCATION)
+                || ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.ACCESS_FINE_LOCATION)
+                || ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                || ActivityCompat.shouldShowRequestPermissionRationale(this,
+                Manifest.permission.READ_PHONE_STATE)
+                ) {
+
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_PHONE_STATE}, BAIDU_READ_PHONE_STATE);
+        }
+        // END_INCLUDE(contacts_permission_request)
     }
 
     @Override
-    public void initActionBar() {
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        switch (requestCode) {
+            // requestCode即所声明的权限获取码，在checkSelfPermission时传入
+            case BAIDU_READ_PHONE_STATE:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    // 获取到权限，作相应处理（调用定位SDK应当确保相关权限均被授权，否则可能引起定位失败）
+                    init();
+                }
+        }
+    }
+
+    private void init() {
+        mMapView = findView(R.id.baidumap);
+        mBaiduMap = mMapView.getMap();
+        mBaiduMap.setMyLocationConfigeration(new MyLocationConfiguration(
+                MyLocationConfiguration.LocationMode.NORMAL, true, null));
+        location();
+
+
+        LatLng llA = new LatLng(39.963175, 116.400244);
+        BitmapDescriptor bdA = BitmapDescriptorFactory
+                .fromResource(R.drawable.icon_gcoding);
+        ArrayList<BitmapDescriptor> giflist = new ArrayList<BitmapDescriptor>();
+        giflist.add(bdA);
+        MarkerOptions options = new MarkerOptions()
+                .position(llA)  //设置marker的位置
+                .icons(giflist)  //设置marker图标
+                .zIndex(9)  //设置marker所在层级
+                .period(10)
+                .draggable(true);  //设置手势拖拽
+        options.animateType(MarkerOptions.MarkerAnimateType.drop);
+        Marker mMarkerD = (Marker) (mBaiduMap.addOverlay(options));
+
+        loadpicture();
+    }
+
+    /**
+     * 路线图
+     */
+    private void loadpicture() {
+        //定义多边形的五个顶点
+        LatLng pt1 = new LatLng(39.93923, 116.357428);
+        LatLng pt2 = new LatLng(39.91923, 116.327428);
+        LatLng pt3 = new LatLng(39.89923, 116.347428);
+        LatLng pt4 = new LatLng(39.89923, 116.367428);
+        LatLng pt5 = new LatLng(39.91923, 116.387428);
+        List<LatLng> pts = new ArrayList<LatLng>();
+        pts.add(pt1);
+        pts.add(pt2);
+        pts.add(pt3);
+        pts.add(pt4);
+        pts.add(pt5);
+//构建用户绘制多边形的Option对象
+        OverlayOptions polygonOption = new PolygonOptions()
+                .points(pts)
+                .stroke(new Stroke(5, 0xAA00FF00))
+                .fillColor(0xAAFFFF00);
+//在地图上添加多边形Option，用于显示
+        mBaiduMap.addOverlay(polygonOption);
+    }
+
+    /**
+     * 定位
+     */
+    private void location() {
+        // 开启定位图层
+        mBaiduMap.setMyLocationEnabled(true);
+        // 定位初始化
+        mLocClient = new LocationClient(this);
+        mLocClient.registerLocationListener(myListener);
+        LocationClientOption option = new LocationClientOption();
+        option.setOpenGps(true); // 打开gps
+        option.setCoorType("bd09ll"); // 设置坐标类型
+        option.setScanSpan(1000);
+        mLocClient.setLocOption(option);
+        mLocClient.start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        // 退出时销毁定位
+        mLocClient.stop();
+        // 关闭定位图层
+        mBaiduMap.setMyLocationEnabled(false);
+        mMapView.onDestroy();
+        mMapView = null;
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onResume() {
+        mMapView.onResume();
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        //在activity执行onPause时执行mMapView. onPause ()，实现地图生命周期管理
+        mMapView.onPause();
+        super.onPause();
+    }
+
+    /**
+     * 定位SDK监听函数
+     */
+    public class MyLocationListenner implements BDLocationListener {
+
+        @Override
+        public void onReceiveLocation(BDLocation location) {
+            // map view 销毁后不在处理新接收的位置
+            if (location == null || mMapView == null) {
+                return;
+            }
+            MyLocationData locData = new MyLocationData.Builder()
+                    .accuracy(location.getRadius())
+                    // 此处设置开发者获取到的方向信息，顺时针0-360
+                    .direction(100).latitude(location.getLatitude())
+                    .longitude(location.getLongitude()).build();
+            mBaiduMap.setMyLocationData(locData);
+            if (isFirstLoc) {
+                isFirstLoc = false;
+                LatLng ll = new LatLng(location.getLatitude(),
+                        location.getLongitude());
+                MapStatus.Builder builder = new MapStatus.Builder();
+                builder.target(ll).zoom(18.0f);
+                mBaiduMap.animateMapStatus(MapStatusUpdateFactory.newMapStatus(builder.build()));
+            }
+        }
+
+        public void onReceivePoi(BDLocation poiLocation) {
+        }
     }
 }
+
