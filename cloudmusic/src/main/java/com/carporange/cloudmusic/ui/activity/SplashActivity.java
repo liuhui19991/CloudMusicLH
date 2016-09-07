a9a036c5ce6e695e150a3c48795da030ed8131d9
liuhui19991 <396604715@qq.com>
2016-09-07 11:02:27 +0800

    更改页面启动动画

--- cloudmusic/src/main/java/com/carporange/cloudmusic/ui/activity/SplashActivity.java
+++ cloudmusic/src/main/java/com/carporange/cloudmusic/ui/activity/SplashActivity.java
@@ -1,27 +1,31 @@
 package com.carporange.cloudmusic.ui.activity;
 
+import android.animation.Animator;
+import android.animation.AnimatorListenerAdapter;
+import android.animation.ObjectAnimator;
+import android.animation.PropertyValuesHolder;
 import android.content.Intent;
-import android.graphics.Color;
-import android.os.Build;
-import android.os.SystemClock;
+import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
-import android.view.animation.AnimationSet;
-import android.view.animation.ScaleAnimation;
-import android.widget.LinearLayout;
+import android.view.animation.AccelerateDecelerateInterpolator;
+import android.widget.TextView;
 
 import com.carporange.cloudmusic.R;
 import com.carporange.cloudmusic.ui.base.BaseActivity;
 
 import butterknife.BindView;
 
+import static com.carporange.cloudmusic.R.id.textView;
+
 /**
  * Created by liuhui on 2016/6/27.
  */
-public class SplashActivity extends BaseActivity {
-    @BindView(R.id.background)
-    LinearLayout mLinearLayout;
+public class SplashActivity extends BaseActivity implements Runnable {
+    @BindView(R.id.fab)
+    FloatingActionButton fab;
+    @BindView(R.id.textView)
+    TextView nameTextView;
 
     @Override
     protected int getLayoutId() {
@@ -35,7 +39,8 @@ public class SplashActivity extends BaseActivity {
 
     @Override
     public void initViews() {
-        AlphaAnimation alphaAnimation = new AlphaAnimation(0f, 1.0f);
+        fab.postDelayed(this, 200);
+        /*AlphaAnimation alphaAnimation = new AlphaAnimation(0f, 1.0f);
         alphaAnimation.setDuration(2000);
         ScaleAnimation scaleAnimation = new ScaleAnimation(0.6f, 1.0f, 0.6f, 1f,
                 Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
@@ -60,6 +65,41 @@ public class SplashActivity extends BaseActivity {
             public void onAnimationRepeat(Animation animation) {
 
             }
+        });*/
+    }
+
+    @Override
+    public void run() {
+        final View parentView = (View) fab.getParent();
+        float scale = (float) (Math.sqrt(parentView.getHeight() * parentView.getHeight() + parentView.getWidth() * parentView.getWidth()) / fab.getHeight());
+        PropertyValuesHolder scaleX = PropertyValuesHolder.ofFloat("scaleX", scale);
+        PropertyValuesHolder scaleY = PropertyValuesHolder.ofFloat("scaleY", scale);
+        ObjectAnimator objectAnimator = ObjectAnimator.ofPropertyValuesHolder(fab, scaleX, scaleY).setDuration(1900);
+        objectAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        objectAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+//                parentView.setBackgroundColor(ContextCompat.getColor(SplashActivity.this, R.color.colorPrimary));
+//                fab.setVisibility(View.GONE);
+//                nameTextView.setVisibility(View.VISIBLE);
+            }
         });
+        PropertyValuesHolder holderA = PropertyValuesHolder.ofFloat("alpha", 0, 1);
+        PropertyValuesHolder holderYm = PropertyValuesHolder.ofFloat("translationY", 0, 300);
+        ObjectAnimator textAnimator = ObjectAnimator.ofPropertyValuesHolder(textView, holderA, holderYm).setDuration(1000);
+        textAnimator.setInterpolator(new AccelerateDecelerateInterpolator());
+        textAnimator.setStartDelay(800);
+
+        textAnimator.addListener(new AnimatorListenerAdapter() {
+            @Override
+            public void onAnimationEnd(Animator animation) {
+                super.onAnimationEnd(animation);
+                startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                finish();
+            }
+        });
+        objectAnimator.start();
+        textAnimator.start();
     }
 }
