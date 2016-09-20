d8126a53e5cd89e47909f44ff960e87294afe652
liuhui19991 <396604715@qq.com>
2016-09-20 10:16:45 +0800

    增加方windows的进度条

--- cloudmusic/src/main/java/com/carporange/cloudmusic/fragment/MusicFragment.java
+++ cloudmusic/src/main/java/com/carporange/cloudmusic/fragment/MusicFragment.java
@@ -1,31 +1,43 @@
 package com.carporange.cloudmusic.fragment;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.carporange.cloudmusic.R;
 import com.carporange.cloudmusic.ui.base.BaseFragment;
+import com.carporange.cloudmusic.util.L;
+import com.carporange.cloudmusic.widget.WinLoading;
+
+import butterknife.BindView;
 
 /**
  * Created by liuhui on 2016/6/27.
  */
 public class MusicFragment extends BaseFragment {
+    @BindView(R.id.waiting_dialog_windows)
+    WinLoading mWinLoading;
 
     public MusicFragment() {
     }
 
 
-    @Override
+    /*@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         return super.onCreateView(inflater, container, savedInstanceState);
-    }
+    }*/
 
     @Override
     protected void onVisible() {
-
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mWinLoading.setVisibility(View.GONE);
+            }
+        }, 2000);
     }
 
     @Override
