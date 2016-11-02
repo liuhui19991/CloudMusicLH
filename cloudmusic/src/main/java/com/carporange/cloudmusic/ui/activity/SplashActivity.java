d084ce606e53064da68f59d1a222dd3bfbf180a8
liuhui19991 <396604715@qq.com>
2016-11-02 09:41:25 +0800

    闪屏也隐藏navigation(底部栏)

--- cloudmusic/src/main/java/com/carporange/cloudmusic/ui/activity/SplashActivity.java
+++ cloudmusic/src/main/java/com/carporange/cloudmusic/ui/activity/SplashActivity.java
@@ -5,8 +5,8 @@ import android.animation.AnimatorListenerAdapter;
 import android.animation.ObjectAnimator;
 import android.animation.PropertyValuesHolder;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
-import android.support.v4.content.ContextCompat;
 import android.view.View;
 import android.view.animation.AccelerateDecelerateInterpolator;
 import android.widget.TextView;
@@ -26,6 +26,20 @@ public class SplashActivity extends BaseActivity implements Runnable {
     FloatingActionButton fab;
     @BindView(R.id.textView)
     TextView nameTextView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //隐藏navigation即底部栏
+        getWindow().getDecorView().
+                setSystemUiVisibility(
+                        View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                                | View.SYSTEM_UI_FLAG_IMMERSIVE);
+    }
 
     @Override
     protected int getLayoutId() {
