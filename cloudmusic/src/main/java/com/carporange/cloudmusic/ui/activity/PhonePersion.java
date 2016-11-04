384c480bf921621b62611e68e3410f370ec436e5
liuhui19991 <396604715@qq.com>
2016-11-04 09:37:10 +0800

    使用CollapsingToolbarLayout

--- cloudmusic/src/main/java/com/carporange/cloudmusic/ui/activity/PhonePersion.java
+++ cloudmusic/src/main/java/com/carporange/cloudmusic/ui/activity/PhonePersion.java
@@ -1,20 +1,46 @@
 package com.carporange.cloudmusic.ui.activity;
 
+import android.graphics.Color;
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.WindowManager;
+
 import com.carporange.cloudmusic.R;
-import com.carporange.cloudmusic.ui.base.BaseActivity;
 
 /**
- * Created by liuhui on 2016/11/3.
+ * Created by liuhui n 2016/11/3.
  */
 
-public class PhonePersion extends BaseActivity {
+public class PhonePersion extends AppCompatActivity {
     @Override
-    protected int getLayoutId() {
-        return R.layout.activity_phone_persion;
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_phone_persion);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        toolbar.setTitle("");
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        initWindow();
+        CollapsingToolbarLayout coordinatorLayout = (CollapsingToolbarLayout) findViewById(R.id.toolbar_layout);
+        coordinatorLayout.setTitle("我饿阿萨德");
     }
 
-    @Override
-    public void initViews() {
-
+    protected void initWindow() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {//5.0及以上
+            View decorView = getWindow().getDecorView();
+            int option = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                    | View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            decorView.setSystemUiVisibility(option);
+            //5.0以上可以直接设置 statusbar颜色
+            getWindow().setStatusBarColor(Color.TRANSPARENT);
+        } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {//4.4到5.0
+            WindowManager.LayoutParams localLayoutParams = getWindow().getAttributes();
+            localLayoutParams.flags = (WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS | localLayoutParams.flags);
+        }
     }
 }
